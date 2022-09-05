bed554c3c108a7e76607894037d0773d2819dcdf
dimaniagara <ds.sugak@gmail.com>
2022-09-04 20:09:49 -0400

    Updates

---
+++ src/components/ContactsItem/ContactsItem.jsx
@@ -0,0 +1,12 @@
+import { ContactsItemWrapper } from './ContactsItem.styled';
+
+export const ContactsItem = ({ name, number, id, deleteContact }) => {
+  return (
+    <ContactsItemWrapper>
+      <p>
+        {name}: {number}
+      </p>
+      <button onClick={() => deleteContact(id)}>Delete</button>
+    </ContactsItemWrapper>
+  );
+};
